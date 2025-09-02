f2bdf5909052fec90b9b2ee2f6690cb34304e302
andrecalegari <andre.calegari.rodrigues@gmail.com>
2025-09-02 15:20:41 -0300

    sdf

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/apostar/", // importante para funcionar no GitHub Pages
+  //base: "/apostar/", // importante para funcionar no GitHub Pages
+  base: "./", // importante para GitHub Pages
+  build: {
+    outDir: 'dist'
+  }
 });
